d7c0264b95bbd5b4a81c95e6f1537f9966c46ca5
Bruno Moura <brunomoura.blm@gmail.com>
2021-05-16 20:29:40 -0300

    Inserindo ID com uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,18 +1,23 @@
 import { Router } from "express";
+import { v4 as uuid } from "uuid";
 
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (request, response) => {
+categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  categories.push({
+  const category = {
+    id: uuid(),
     name,
     description,
-  });
+    created_at: new Date(),
+  };
 
-  return response.status(201).send();
+  categories.push(category);
+
+  return response.status(201).json(categories);
 });
 
 export { categoriesRoutes };
