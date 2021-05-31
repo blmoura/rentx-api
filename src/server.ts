dd649dc29f526d8861070abee55229b04ac5f873
Bruno Moura <brunomoura.blm@gmail.com>
2021-05-30 22:44:34 -0300

    Conhecendo TSyringe

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
 
+import "./database";
+
+import "./shared/container";
+
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
-
-import "./database";
 
 const app = express();
 
